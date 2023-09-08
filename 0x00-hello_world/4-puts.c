53fb2a843fa35fc2ef7010faf7bb4e4dd49fcabd
Bafffzz <baffygyimah23@gmail.com>
2023-09-07 21:38:46 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with function
- * Return: 0 (Success)
+ *
+ * Return: Return 0 for success
  */
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
 }
