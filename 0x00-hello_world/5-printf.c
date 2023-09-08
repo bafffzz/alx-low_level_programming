5b80e6b85acb2f958ee74d7fb6aeb2f2b9b1a5ef
Bafffzz <baffygyimah23@gmail.com>
2023-09-07 21:51:28 -0700

    Betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- *  Return: always 0 (SUccess)
+ * main - Entry point
+ *  Return: always 0 (Success)
  *
  *
  */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
 }
